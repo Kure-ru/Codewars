6c62e98f8ab3d55f9b79765e1f96420642063d53
Kure-ru <clairedochez@gmail.com>
2023-04-13 11:55:02 +0900

    230413

---
+++ 5kyu/Moving_Zeros_To_The_End.js
@@ -0,0 +1,10 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(arr) {
+  let filteredArr = arr.filter((value) => value !== 0);
+  let newArrLength = filteredArr.length;
+  filteredArr.length = arr.length;
+  return filteredArr.fill(0, newArrLength, arr.length);
+}
