ecb1931cadf8f6b9649bc7ee63b4245f746b6b22
Kure-ru <clairedochez@gmail.com>
2023-05-11 10:36:37 +0900

    050511

---
+++ 8kyu/Define_a_card_suit.js
@@ -0,0 +1,22 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+  let suit = card[card.length - 1];
+  switch (suit) {
+    case "♣":
+      return "clubs";
+    case "♠":
+      return "spades";
+    case "♦":
+      return "diamonds";
+    case "♥":
+      return "hearts";
+  }
+}
