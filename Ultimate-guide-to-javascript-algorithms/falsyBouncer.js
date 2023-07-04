60c70a3d1e07e2dd6ed63ff9405b4d0013d99ca6
Claire Dochez <clairedochez@gmail.com>
2023-07-04 05:00:53 +0200

    230704

---
+++ Ultimate-guide-to-javascript-algorithms/falsyBouncer.js
@@ -0,0 +1,30 @@
+/*
+Given an array, remove all falsy values from the array
+and return an array of only truthy values.
+
+E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
+*/
+
+// Using a for...of loop
+function falsyBouncer(array) {
+  let result = []
+  // loop through with each array value
+  for (value of array){
+    // push into result if truthy
+    if(value){
+        result.push(value)
+    }
+  }
+  return result
+
+}
+
+// Using .filter()
+function falsyBouncer(array) {
+    return array.filter((value) => {
+        return Boolean(value)
+    })
+  
+  }
+
+module.exports = falsyBouncer;
